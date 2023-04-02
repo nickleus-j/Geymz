1cfaf25defd05f60228c9fca41e22faf7e3d895c
Nickleus Jimenez <Nickleus-J@live.com.ph>
2023-04-03 04:54:46 +0800

    Add project files.

---
+++ Geymz.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Geymz.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Geymz.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Snake()
+        {
+            return View();
+        }
+        public IActionResult Pong()
+        {
+            return View();
+        }
+        public IActionResult TicTacToe()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
