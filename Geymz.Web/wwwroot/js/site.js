5818900fb512bf1282534cb2abc06eb49ac92b54
Nickleus Jimenez <Nickleus-J@live.com.ph>
2025-06-11 11:16:01 +0800

    Sound for tic tac toe

--- Geymz.Web/wwwroot/js/site.js
+++ Geymz.Web/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+function pingViaAudio(freq) {
+    var oscillator = audioContext.createOscillator();
+    var gainNode = audioContext.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    oscillator.type = 'sine'; // Or 'triangle', 'square', 'sawtooth'
+    oscillator.frequency.setValueAtTime(freq, audioContext.currentTime); // Frequency in Hz
+
+    // Create a quick decay for a "ping" effect
+    gainNode.gain.setValueAtTime(0.5, audioContext.currentTime); // Initial volume
+    gainNode.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 0.3); // Decay over 0.3 seconds
+
+    oscillator.start(audioContext.currentTime);
+    oscillator.stop(audioContext.currentTime + 0.3); // Stop after the decay
+}
