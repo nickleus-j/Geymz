3dae731150d48c099f4447bc51fb967e55afe0a4
Nickleus Jimenez <Nickleus-J@live.com.ph>
2025-06-14 12:25:35 +0800

    tangram

--- Geymz.Web/Controllers/HomeController.cs
+++ Geymz.Web/Controllers/HomeController.cs
@@ -37,6 +37,10 @@ namespace Geymz.Web.Controllers
         {
             return View();
         }
+        public IActionResult Tangram()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
